d6f71d5483950d93f18d91d4912dcc6f9291c630
Your Name <Zaur Molotnikov>
2017-06-27 04:07:10 -0400

    Account login tests

---
+++ features/step_definitions/user_login.rb
@@ -0,0 +1,31 @@
+When(/^follow register link$/) do
+  click_link('Register')
+end
+
+Then(/^I see registration form$/) do
+  expect(page).to have_field("user_name")
+  expect(page).to have_field("user_email")
+  expect(page).to have_field("user_password")
+  expect(page).to have_field("user_password_confirmation")
+end
+
+Given(/^a user '([^\']*)' with email '([^\']*)'  and password '([^\']*)' exists$/) do |name, email, pass|
+  User.create(:name => name, :email => email, :password => pass, :password_confirmation => pass)
+end
+
+When(/^I go to login page$/) do
+  visit(new_user_session_path)
+end
+
+When(/^I get a login form$/) do
+  expect(page).to have_field("user_email")
+  expect(page).to have_field("user_password")
+end
+
+Then(/^I can login with email '([^\']*)' and password '([^\']*)'$/) do |email, pass|
+  fill_in 'user_email', with: email
+  fill_in 'user_password', with: pass
+  click_link_or_button 'Log in'
+  expect(page).to have_content('Signed in successfully.')
+end
+
