0c061cd23fe62aa2315f38d4d6bb863dc2fad1e5
Your Name <Zaur Molotnikov>
2017-06-27 05:02:16 -0400

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626122352) do
+ActiveRecord::Schema.define(version: 20170627084505) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20170626122352) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+  end
+
+  create_table "vulnerabilities", force: :cascade do |t|
+    t.string "name"
+    t.text "summary"
+    t.datetime "created"
+    t.datetime "modified"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
