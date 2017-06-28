2faccdf46703f15837130cc3a241fac2ec2c88d8
Your Name <Zaur Molotnikov>
2017-06-28 03:35:48 -0400

    Sign in required to view vulns, only updating from nvd, showing and index are allowed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :vulnerabilities
-  get 'nvd_update', to: 'vulnerabilities#nvd', as: 'nvd_update'
+  authenticate :user do
+    resources :vulnerabilities, only: [:index, :show]
+    get 'nvd_update', to: 'vulnerabilities#nvd', as: 'nvd_update'
+  end
 
   devise_for :users
   root to: 'static#home'
+  
 end
