02a3bdf6fe26eabfbfa43f1f4f9ffa6f4e2065d8
Your Name <Zaur Molotnikov>
2017-06-28 10:47:46 -0400

    Project controller added with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   authenticate :user do
     resources :vulnerabilities, only: [:index, :show]
+    resources :projects, only: [:new, :create, :update, :show]
     get 'nvd_update', to: 'vulnerabilities#nvd', as: 'nvd_update'
   end
 
