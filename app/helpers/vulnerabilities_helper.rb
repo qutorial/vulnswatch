2f68593ce511d405e821a1119eeaa86923224f8a
Your Name <Zaur Molotnikov>
2017-06-29 05:21:49 -0400

    link to nvd refactored

--- app/helpers/vulnerabilities_helper.rb
+++ app/helpers/vulnerabilities_helper.rb
@@ -1,7 +1,11 @@
 module VulnerabilitiesHelper
 
-  def link_to_nvd(vuln, title='View on NVD')
-    link_to title, "https://nvd.nist.gov/vuln/detail/#{vuln.name}"
+  def link_to_nvd(vuln, title='View on NVD', other_params = {})
+    link_to title, url_to_nvd(vuln), other_params
+  end
+
+  def url_to_nvd(vuln)
+    "https://nvd.nist.gov/vuln/detail/#{vuln.name}"
   end
 
 end
