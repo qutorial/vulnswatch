ef2d80b303d238571f9ae21a693d36657d60ccb1
Your Name <Zaur Molotnikov>
2017-06-29 04:17:34 -0400

    Registration disabled

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-  	:recoverable, :rememberable, :trackable, :validatable, :lockable, :zxcvbnable #,
-        #:confirmable
+
+  devise :database_authenticatable, 
+        #:omniauthable,
+        #:registerable,
+        #:timeoutable,
+  	:recoverable, 
+  	:rememberable, 
+  	:trackable, 
+  	:validatable, 
+  	:lockable,
+  	#:confirmable,
+        :zxvbnable
+        
   has_many :projects, dependent: :destroy
+  
 end
