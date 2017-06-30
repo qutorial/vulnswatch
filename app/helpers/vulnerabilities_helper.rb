2fb341921d2873c578da0bd286c937cc09456f34
Your Name <Zaur Molotnikov>
2017-06-30 09:56:04 -0400

    New helper to display vulns, better table layout

--- app/helpers/vulnerabilities_helper.rb
+++ app/helpers/vulnerabilities_helper.rb
@@ -1,11 +1,15 @@
 module VulnerabilitiesHelper
 
-  def link_to_nvd(vuln, title='View on NVD', other_params = {})
-    link_to title, url_to_nvd(vuln), other_params
+  def link_to_nvd(vulnerability, title='View on NVD', other_params = {})
+    link_to title, url_to_nvd(vulnerability), other_params
   end
 
-  def url_to_nvd(vuln)
-    "https://nvd.nist.gov/vuln/detail/#{vuln.name}"
+  def url_to_nvd(vulnerability)
+    "https://nvd.nist.gov/vuln/detail/#{vulnerability.name}"
+  end
+  
+  def link_to_vulnerability(vulnerability)
+    link_to vulnerability.name, vulnerability, title: vulnerability.summary
   end
 
 end
