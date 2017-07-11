48d00ebc7b2f985c103087e7ea3e6c5ea9722753
Zaur <zaur@genua.de>
2017-07-11 13:44:02 +0200

    Backup script fixed

--- backup.sh
+++ backup.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 BACKUPDIR=~/backups_sahare
-BACKUPFILE="$BACKUPDIR/$name.pgdump"
+name=$(date '+%Y-%m-%d-%H:%M:%S')
+BACKUPFILE=$BACKUPDIR"/"$name".pgdump"
 APP=tranquil-caverns-87454
 DATASTOREURL=https://data.heroku.com/datastores/dc742fea-b2eb-48fe-9477-189f1c28a034
 mkdir -p "$BACKUPDIR"
-name=$(date '+%y-%m-%d-%H-%M')
 heroku pg:backups:capture
 heroku pg:backups:download -a $APP -o "$BACKUPFILE"
 rc=$?;
