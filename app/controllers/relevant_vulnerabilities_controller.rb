1d37b76b6f3c460dda5aa49542d37827e6beb378
Zaur <zaur@genua.de>
2017-07-11 13:37:41 +0200

    Syntax fixed

--- app/controllers/relevant_vulnerabilities_controller.rb
+++ app/controllers/relevant_vulnerabilities_controller.rb
@@ -1,16 +1,19 @@
 class RelevantVulnerabilitiesController < ApplicationController
 
-  def index
-    (current_user.projects.select 
-            { |project| 
-              RelevantVulnerability.is_a_huge_project(project) 
-             }).each do |huge_project|
+  def index()
+    huge_projects = current_user.projects.select { 
+       |project| RelevantVulnerability.is_a_huge_project?(project)} 
+    
+    huge_projects.each do |huge_project|
       flash[:alert] = "The project '#{huge_project.name}' is too big. Please, reduce the number of systems there.\n"
     end
-    if RelevantVulnerability.has_too_many_systems?(current_user)
+ 
+   if RelevantVulnerability.has_too_many_systems?(current_user)
       flash[:alert] = "You have too many system in your projects. Please, reduce their count."
     end
+
     @relevant_vulnerabilities = RelevantVulnerability.relevant_vulnerabilities(current_user).paginate(page: params[:page])    
+
   end
 
 end
