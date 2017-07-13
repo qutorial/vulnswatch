3fefcc88287034338cb934d5213cb1db1ad68527
Zaur <zaur@genua.de>
2017-07-13 14:47:14 +0200

    Cucumber test for vulnerabilities

---
+++ features/step_definitions/vulnerabilities.rb
@@ -0,0 +1,80 @@
+Given(/^the following vulnerabilities exist:$/) do |table|
+  table.hashes.each do |vuln|
+    Vulnerability.create(vuln)
+  end
+end
+
+Given(/^following users exist:$/) do |table|
+  table.hashes.each do |user|
+    User.create(name: user[:name], email: user[:email], password: user[:password], password_confirmation: user[:password])
+  end
+end
+
+Given(/^the following projects exist:$/) do |table|
+  table.hashes.each do |project|
+    user = User.find_by(name: project[:user])
+    user.projects.build(name: project[:name], 
+        systems_description: project[:description]).save!
+  end
+end
+
+Given(/^'([^\']*)' has logged in with '([^\']*)' and '([^\']*)'$/) do |user, email, password|
+ step "I can login with email '#{email}' and password '#{password}'"
+end
+
+When(/^I go to vulnerabilities page$/) do
+  visit vulnerabilities_path
+end
+
+Then(/^I should (not )?see vulnerability (.*)$/) do |notsee, vuln|
+  if notsee.nil?
+    expect(page).to have_content(vuln)
+  else
+    expect(page).not_to have_content(vuln)
+  end
+end
+
+Then(/^I should (not )?see vulnerabilities: (.*)$/) do |notsee, vulns|
+  vulnerabilities = vulns.split(/, ?/)
+  vulnerabilities.each do |vuln|
+    step "I should #{notsee}see vulnerability #{vuln}"
+  end
+end
+
+
+#When(/^follow register link$/) do
+#  click_link('Register')
+#end
+
+#Then(/^I see registration form$/) do
+#  expect(page).to have_field("user_name")
+#  expect(page).to have_field("user_email")
+#  expect(page).to have_field("user_password")
+#  expect(page).to have_field("user_password_confirmation")
+#end
+
+#Given(/^a user '([^\']*)' with email '([^\']*)'  and password '([^\']*)' exists$/) do |name, email, pass|
+#  User.create(:name => name, :email => email, :password => pass, :password_confirmation => pass)
+#end
+
+#When(/^I go to login page$/) do
+#  visit(new_user_session_path)
+#end
+
+#When(/^I get a login form$/) do
+#  expect(page).to have_field("user_email")
+#  expect(page).to have_field("user_password")
+#end
+
+#Then(/^I can (not )?login with email '([^\']*)' and password '([^\']*)'$/) do |cannot, email, pass|
+#  step "I go to login page"
+#  fill_in 'user_email', with: email
+#  fill_in 'user_password', with: pass
+#  click_link_or_button 'Log in'
+#  if cannot.nil?
+#    expect(page).to have_content('Signed in successfully.')
+#  else
+#    expect(page).to have_content('Invalid Email or password.')
+#  end
+#end
+
---
+++ features/vulnerabilities.feature
@@ -0,0 +1,31 @@
+Feature: Showing Vulnerabilities
+  As a vulnerabilty manager
+  I want to see vulnerabilities, relevant vulnerabilities,
+  sort them and filter them.
+  
+Background: vulnerabilities added, user created and logged in
+
+  Given the following vulnerabilities exist:
+  | name	| summary			| created       | modified   | affected_system	|
+  | CVE-2017-1  | OpenBSD vulnerability		| 2017-01-01	| 2017-01-01 |			|
+  | CVE-2017-2  | Perl vulnerability		| 2017-01-02	| 2017-01-02 |			|
+  | CVE-2017-3  | Microsoft vulnerability	| 2017-01-01	| 2017-01-01 |			|
+  | CVE-2017-4  | Known OpenBSD vulnerability	| 2017-01-01	| 2017-01-01 | OpenBSD		|
+  | CVE-2017-5  | Python CLI bug		| 2017-01-01	| 2017-01-01 | Python		|
+  | CVE-2017-6  | Wordpress v.1.1 affected	| 2017-01-01	| 2017-01-01 |			|
+  
+  Given following users exist:
+  | name		| email			| password	|
+  | John Smith		| john@smith.com	| JohnIsAV3rySm0rtG4y |
+  
+  Given the following projects exist:
+  | name		| user			| description			|
+  | Workstation		| John Smith		| OpenBSD, perl			|
+  | Webserver		| John Smith		| OpenBSD, python, Wordpress	|
+
+  Given 'John Smith' has logged in with 'john@smith.com' and 'JohnIsAV3rySm0rtG4y'
+
+
+Scenario: All vulnerabilities can be seen
+  When I go to vulnerabilities page
+  Then I should see vulnerabilities: CVE-2017-1, CVE-2017-2, CVE-2017-2, CVE-2017-3, CVE-2017-4, CVE-2017-5, CVE-2017-6
