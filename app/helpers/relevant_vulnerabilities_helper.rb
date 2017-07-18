c694d67f53e0a4a43cdf340b614e37a037c690eb
Zaur <zaur@genua.de>
2017-07-18 10:46:24 +0200

    Fixed double escaping

--- app/helpers/relevant_vulnerabilities_helper.rb
+++ app/helpers/relevant_vulnerabilities_helper.rb
@@ -8,7 +8,7 @@ module RelevantVulnerabilitiesHelper
             String.new(h(system)) + 
             " in " + 
                     RelevantVulnerability.projects_having_system(system, user)
-                                  .map(  &->(p){ link_to String.new('\''+h(p.name)+'\''), p } )
+                                  .map(  &->(p){ link_to p.name, p } )
                             .join(' and in ')
     end
     return res unless res.empty?
