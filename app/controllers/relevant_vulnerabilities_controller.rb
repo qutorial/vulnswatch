51aa5c44c7793f7960ebff8c1b3355a40af88e5d
Zaur <zaur@genua.de>
2017-07-18 15:51:04 +0200

    Relecant vulnerabilities view and controller removed

--- app/controllers/relevant_vulnerabilities_controller.rb
+++ app/controllers/relevant_vulnerabilities_controller.rb
@@ -1,19 +1,7 @@
 class RelevantVulnerabilitiesController < ApplicationController
 
   def index()
-    huge_projects = current_user.projects.select { 
-       |project| RelevantVulnerability.is_a_huge_project?(project)} 
-    
-    huge_projects.each do |huge_project|
-      flash[:alert] = "The project '#{huge_project.name}' is too big. Please, reduce the number of systems there.\n"
-    end
- 
-   if RelevantVulnerability.has_too_many_systems?(current_user)
-      flash[:alert] = "You have too many system in your projects. Please, reduce their count."
-    end
-
-    @relevant_vulnerabilities = RelevantVulnerability.all_relevant_vulnerabilities_for_user(current_user).paginate(page: params[:page])    
-
+    redirect_to vulnerabilities_path(project: 0)
   end
 
 end
