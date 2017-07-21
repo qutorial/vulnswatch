e4da2043dead418a1bd79820ca0a0be0be6802c5
Zaur <zaur@genua.de>
2017-07-21 20:32:43 +0200

    Some tests fixed

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,5 @@ class Tag < ApplicationRecord
   validates :user_id, presence: true
   validates :vulnerability_id, presence: true
   validates_uniqueness_of :component, scope: :vulnerability_id
+  validates :component, presence: true, allow_blank: false
 end
