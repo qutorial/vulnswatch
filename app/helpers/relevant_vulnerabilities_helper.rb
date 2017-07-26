81062eddf8766084467a1770b426224fe4cdf550
Zaur <zaur@genua.de>
2017-07-26 19:49:30 +0200

    Explanations made faster

--- app/helpers/relevant_vulnerabilities_helper.rb
+++ app/helpers/relevant_vulnerabilities_helper.rb
@@ -1,17 +1,2 @@
-require 'set'
-
 module RelevantVulnerabilitiesHelper
-  def why_relevant(vulnerability, user)
-    res = ''
-    RelevantVulnerability.affected_systems(vulnerability, user).each do |system|
-      res += (res.empty? ? '' : '<br/> and ') + 
-            String.new(h(system)) + 
-            " in " + 
-                    RelevantVulnerability.projects_having_system(system, user)
-                                  .map(  &->(p){ link_to p.name, p } )
-                            .join(' and in ')
-    end
-    return res unless res.empty?
-    return '-'
-  end
 end
