a3c34f0dcd2019ecb5b52d6fc1dfd2976da9e3e0
Zaur <zaur@genua.de>
2017-07-26 17:38:59 +0200

    Disabled why relevant

--- app/helpers/relevant_vulnerabilities_helper.rb
+++ app/helpers/relevant_vulnerabilities_helper.rb
@@ -3,6 +3,7 @@ require 'set'
 module RelevantVulnerabilitiesHelper
   def why_relevant(vulnerability, user)
     res = ''
+    return 'explanations disabled'
     RelevantVulnerability.affected_systems(vulnerability, user).each do |system|
       res += (res.empty? ? '' : '<br/> and ') + 
             String.new(h(system)) + 
