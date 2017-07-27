d8a8868bc4c1acf0535af7ac6757cdc50c825fb8
Zaur <zaur@genua.de>
2017-07-27 17:03:09 +0200

    why relevant

--- app/helpers/relevant_vulnerabilities_helper.rb
+++ app/helpers/relevant_vulnerabilities_helper.rb
@@ -1,2 +1,17 @@
-module RelevantVulnerabilitiesHelper
-end
+require 'set'
+
+ module RelevantVulnerabilitiesHelper
+  def why_relevant(vulnerability, user)
+    res = ''
+    RelevantVulnerability.affected_systems(vulnerability, user).each do |system|
+      res += (res.empty? ? '' : '<br/> and ') + 
+            String.new(h(system)) + 
+            " in " + 
+                    RelevantVulnerability.projects_having_system(system, user)
+                                  .map(  &->(p){ link_to p.name, p } )
+                            .join(' and in ')
+    end
+    return res unless res.empty?
+    return '-'
+  end
+ end
