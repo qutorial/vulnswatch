58c39a45c09e4929de0dfb37bfdcb2be3506b2af
Zaur <zaur@genua.de>
2017-08-05 13:05:37 +0200

    Management optimized to be faster

--- app/models/management.rb
+++ app/models/management.rb
@@ -11,8 +11,8 @@ class Management < ApplicationRecord
       raise ArgumentError, "Trying to delete not so old records, use force if you really want it."
     end
 
-    vulns_to_delete = Vulnerability.where('modified <= ?', date_threshold)
-    vulns_to_delete.each do |vuln|
+    vulns_to_delete = Vulnerability.where('modified <= ?', date_threshold).includes(:tags).includes(:reactions)
+    vulns_to_delete.find_each do |vuln|
       next if vuln.tags.count > 0
       next if vuln.reactions.count > 0
       next if User.any?(&->(user){RelevantVulnerability.affects_user?(vuln,user)})
