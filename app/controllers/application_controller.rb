c162d8909e6c6daad32704ed6fd870a77cb2aee6
Zaur <zaur@genua.de>
2017-08-05 12:53:54 +0200

    Handling heroku freaking out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,20 +13,22 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
       respond_to do |format|
-        format.json { head :forbidden, content_type: 'text/html' }
         format.html { redirect_to main_app.root_url, notice: exception.message }
-        format.js   { head :forbidden, content_type: 'text/html' }
       end
   end  
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
       respond_to do |format|
-        format.json { head :forbidden, content_type: 'text/html' }
         format.html { redirect_to main_app.root_url, notice: exception.message }
-        format.js   { head :forbidden, content_type: 'text/html' }
       end
   end  
-
+  
+  rescue_from ActiveRecord::StatementInvalid do |exception|
+      respond_to do |format|
+        format.html { redirect_to main_app.root_url, notice: exception.message }
+      end
+  end
+  
   def alert_on_huge_projects
     current_user.projects.each do |project|
       if RelevantVulnerability.is_a_huge_project?(project)
